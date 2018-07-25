53debf1912b24d9af4c21ce48be9340b2933e07f
Jaime Coue <jaimecoue@gmail.com>
2018-07-25 17:32:34 +0200

    Initial Commit
    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$('#myAffix').affix({
+  offset: {
+    top: 100,
+    bottom: function () {
+      return (this.bottom = $('.footer').outerHeight(true))
+    }
+  }
+})
+
+$('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
+
+var animateHTML = function () {
+  var elems,
+    windowHeight
+  var init = function () {
+    elems = document.getElementsByClassName('hidden')
+    windowHeight = window.innerHeight
+    _addEventHandlers()
+  }
+  var _addEventHandlers = function () {
+    window.addEventListener('scroll', _checkPosition)
+    window.addEventListener('resize', init)
+  }
+  var _checkPosition = function () {
+    for (var i = 0; i < elems.length; i++) {
+      var posFromTop = elems[i].getBoundingClientRect().top
+      if (posFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace('hidden', 'fade-in-element')
+      }
+    }
+  }
+  return {
+    init: init
+  }
+}
+animateHTML().init()
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
